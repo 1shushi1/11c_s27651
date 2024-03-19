70291460499faac062b3bb0efb3eac861f86b425
1shushi1 <lytvynyuk.oleksiy1@gmail.com>
2024-03-19 18:46:13 +0100

    Task eight.

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,4 +1,3 @@
-
 """
 Task 3: Classes
 Create a class called SquareGenerator that has a method to generate squares for a given range of numbers.
@@ -8,6 +7,7 @@ Create a class called SquareGenerator that has a method to generate squares for 
 Task 6: Modules
 Extract the SquareGenerator class into a separate module named square_generator.py.
 """
+
 
 class SquareGenerator:
     def squares_generator(self, start, end):
@@ -26,3 +26,17 @@ class CubicGenerator(SquareGenerator):
     def cubes_generator(self, start, end):
         cubes = [i ** 3 for i in range(start, end)]
         return cubes
+
+"""
+Task 9: Function Overriding
+Override the square generation method in the Cubic Generator class to generate squares with a check to see if the start of the range is less than the end, 
+if not return an Exceptions
+"""
+
+
+# class CubicGenerator(SquareGenerator):
+#     def squares_generator(self, start, end):
+#         if start < end:
+#             squares = [i ** 2 for i in range(start, end)]
+#         else:
+#             raise ValueError("End value must be grater than start value")
